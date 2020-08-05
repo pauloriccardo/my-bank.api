005db8e663423e72a1145121f9bf9c330cf3e9fb
Paulo Riccardo <pauloriccardo@hotmail.com>
2020-08-05 19:10:31 -0300

    add procfile

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ global.logger = winston.createLogger({
     format: combine(label({ label: 'my-bank-api' }), timestamp(), myFormat),
 });
 
-app.listen(3000, async () => {
+app.listen(5000, async () => {
     try {
         await fs.readFile(global.fileName, 'utf8');
         logger.info('API start');
